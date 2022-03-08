c1684cbd6cd59227793da9b398ca3859bca0f9fa
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2022-03-08 16:16:01 +0100

    just creating the project

---
+++ tp1-orm/src/main/java/me/obelmokhtar/tp1orm/Tp1OrmApplication.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.tp1orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1OrmApplication.class, args);
+    }
+
+}
---
+++ tp1-orm/src/test/java/me/obelmokhtar/tp1orm/Tp1OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.tp1orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1OrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
